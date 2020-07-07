3b81d65dca2a94205ea18752867e152a998c375c
Pavel Martynov <martynovdev@gmail.com>
2020-07-07 18:00:49 +0300

    Add data placeholders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/ui/Header';
+
+function Episodes (props) {
+  return (
+    <div className='episodes'>
+      <div className='episodes__item'>placeholder</div>
+      <div className='episodes__item'>placeholder</div>
+      <div className='episodes__item'>placeholder</div>
+      <div className='episodes__linebreak'></div>
+      <div className='episodes__item'>placeholder</div>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <Episodes />
     </div>
   );
 }
